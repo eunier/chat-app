4977a3aed31853e7799cec3f89b4bb34dac01990
Yunier Alvarez <yunieralvarez@protonmail.com>
2019-06-19 00:11:42 -0400

    Able to load index.html

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-import express from 'express';
-import { createServer } from 'http';
-import { listen } from 'socket.io';
-
+const express = require('express');
 const app = express();
-const server = createServer(app);
-const io = listen(server);
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+
+const port = process.env.PORT || 3000;
+server.listen(port);
+console.log(`server running ${port}`);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
